ef81fb4a15f3cdd68779734356af8ec8fdde173a
MD Ziauddin <md.ziauddin02@gmail.com>
2024-02-16 15:03:12 +0530

    feat(mui): implements mui and it's theme config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+
 import "./globals.css";
+
+import theme from "@/app/_utils/theme/theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
---
+++ src/components/CustomSlider.tsx
@@ -0,0 +1,11 @@
+import Switch from "@mui/material/Switch";
+const CustomSlider = ({ warning }: { warning: boolean }) => (
+  <Switch
+    sx={{
+      "& .MuiSwitch-thumb": {
+        color: warning ? "#D66460" : "#A3A3D3",
+      },
+    }}
+  />
+);
+export default CustomSlider;
